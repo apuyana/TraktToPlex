a636bbbf0e7415ab10f3e014d31c5f1c5858d230
Alberto Puyana <apuyana@anuracode.com>
2020-03-20 14:18:57 -0500

    Two way movie sync

--- SyncClient/SyncAgent.cs
+++ SyncClient/SyncAgent.cs
@@ -4,8 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TraktNet;
+using TraktNet.Objects.Get.Collections;
 using TraktNet.Objects.Get.Movies;
 using TraktNet.Objects.Get.Watched;
+using TraktNet.Objects.Post.Syncs.Collection;
+using TraktNet.Objects.Post.Syncs.History;
 using TraktNet.Requests.Parameters;
 using TraktNet.Responses;
 using Plex = PlexClient;
@@ -14,6 +17,11 @@ namespace SyncClient
 {
     public class SyncAgent
     {
+        /// <summary>
+        /// List of process movies so far.
+        /// </summary>
+        private readonly List<ITraktMovie> processedMovies = new List<ITraktMovie>(200);
+
         /// <summary>
         /// Batch limit.
         /// </summary>
@@ -48,19 +56,30 @@ namespace SyncClient
         /// <returns>Task to await.</returns>
         public async Task SyncMoviesAsync()
         {
+            if (processedMovies.Count > 0)
+            {
+                processedMovies.Clear();
+            }
+
             Task<Movie[]> plexMoviesTask = PlexClient.GetMovies();
 
-            Task<TraktListResponse<ITraktWatchedMovie>> traktMoviesTask = TraktClient.Sync.GetWatchedMoviesAsync(new TraktExtendedInfo().SetFull());
+            // Get all trakt colleciton.
+            Task<TraktListResponse<ITraktCollectionMovie>> traktMoviesTask = TraktClient.Sync.GetCollectionMoviesAsync(new TraktExtendedInfo().SetMetadata());
 
-            await Task.WhenAll(plexMoviesTask, traktMoviesTask);
+            Task<TraktListResponse<ITraktWatchedMovie>> traktMoviesWatchedTask = TraktClient.Sync.GetWatchedMoviesAsync(new TraktExtendedInfo().SetMetadata());
 
-            ITraktWatchedMovie[] traktMovies = traktMoviesTask.Result.ToArray();
+            await Task.WhenAll(plexMoviesTask, traktMoviesWatchedTask, traktMoviesTask);
+
+            ITraktCollectionMovie[] traktMovies = traktMoviesTask.Result.ToArray();
+            ITraktWatchedMovie[] traktMoviesWatched = traktMoviesWatchedTask.Result.ToArray();
             Movie[] plexMovies = plexMoviesTask.Result;
 
-            if ((traktMovies != null) && (traktMovies.Length > 0) &&
+            // Sync plex movies to trakt.
+            if ((traktMoviesWatched != null) && (traktMoviesWatched.Length > 0) &&
+                (traktMovies != null) && (traktMovies.Length > 0) &&
                 (plexMovies != null) && (plexMovies.Length > 0))
             {
-                await ReportProgressAsync($"Total watched plex movies: {plexMovies.Length}; Total watched Trakt Movies {traktMovies.Length}");
+                await ReportProgressAsync($"Total plex movies: {plexMovies.Length}; Total watched Trakt Movies {traktMoviesWatched.Length}");
 
                 int calculatedLimit = plexMovies.Length;
                 int i;
@@ -78,7 +97,7 @@ namespace SyncClient
                     {
                         if ((i + j) < calculatedLimit)
                         {
-                            batchTasks.Add(ProcessPlexMovieAsync(plexMovie: plexMovies[i + j], traktMovies: traktMovies));
+                            batchTasks.Add(ProcessPlexMovieAsync(plexMovie: plexMovies[i + j], traktMoviesWatched: traktMoviesWatched, traktMovies: traktMovies));
                         }
                     }
 
@@ -87,6 +106,46 @@ namespace SyncClient
                     await ReportProgressAsync(string.Empty);
                     await ReportProgressAsync($"------- Processed from: {i} to {i + batchLimit}. Total to process: {calculatedLimit} ----------");
                 }
+            }
+
+            await ReportProgressAsync($"------- All processed, Plex movies: {plexMovies.Length}; Trakt movies: {traktMovies.Length}; Processed trakt movies found: {processedMovies.Count}");
+
+            if (DeleteFromCollection && traktMovies.Length > 0 && processedMovies.Count > 0)
+            {
+                List<ITraktMovie> deleteQueueMovies = new List<ITraktMovie>(traktMovies.Length);
+
+                for (int i = 0; i < traktMovies.Length; i++)
+                {
+                    var traktMovieProcessFound = processedMovies.FirstOrDefault(x => HasMatchingId(traktMovies[i].Ids, x.Ids));
+
+                    if (traktMovieProcessFound == null)
+                    {
+                        // Add to delete list.
+
+                        deleteQueueMovies.Add(traktMovies[i]);
+                        await ReportProgressAsync($"------- Movie {traktMovies[i].Title} should be removed from collection");
+                    }
+                }
+
+                await ReportProgressAsync($"------- Trakt movies to remove from collection: {deleteQueueMovies.Count}");
+
+                if (deleteQueueMovies.Count > 0)
+                {
+                    TraktSyncCollectionPostBuilder tsp = new TraktSyncCollectionPostBuilder();
+                    tsp.AddMovies(deleteQueueMovies);
+                    await TraktClient.Sync.RemoveCollectionItemsAsync(tsp.Build());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Flag to check if the extra item should be removed from collection.
+        /// </summary>
+        protected bool DeleteFromCollection
+        {
+            get
+            {
+                return false;
             }
         }
 
@@ -112,20 +171,68 @@ namespace SyncClient
         }
 
         /// <summary>
+        /// Predicate to match a movie.
+        /// </summary>
+        /// <param name="traktIds1">Trakt Ids.</param>
+        /// <param name="traktIds2">Trakt Ids.</param>
+        /// <returns>True if match.</returns>
+        private bool HasMatchingId(ITraktMovieIds traktIds1, ITraktMovieIds traktIds2)
+        {
+            return traktIds1.HasAnyId && traktIds2.HasAnyId && traktIds1.Trakt == traktIds2.Trakt;
+
+
+        }
+
+        /// <summary>
         /// Process single moview.
         /// </summary>
         /// <param name="plexMovie">Plex movie to sync.</param>
-        /// <param name="traktMovies">All Trakt movies to search.</param>
+        /// <param name="traktMovies">All movie collection.</param>
+        /// <param name="traktMoviesWatched">All Trakt movies to search.</param>
         /// <returns>Task to await.</returns>
-        private async Task ProcessPlexMovieAsync(Movie plexMovie, ITraktWatchedMovie[] traktMovies)
+        private async Task ProcessPlexMovieAsync(Movie plexMovie, ITraktCollectionMovie[] traktMovies, ITraktWatchedMovie[] traktMoviesWatched)
         {
-            var traktMovie = traktMovies.FirstOrDefault(x => HasMatchingId(plexMovie, x.Ids));
-            if (traktMovie == null)
+            var traktMovieWatched = traktMoviesWatched.FirstOrDefault(x => HasMatchingId(plexMovie, x.Ids));
+            if (traktMovieWatched == null)
             {
-                await ReportProgressAsync($"The movie \"{plexMovie.Title}\" was not found as watched on Trakt. Skipping!");
+                await ReportProgressAsync($"The movie \"{plexMovie.Title}\" was not found as watched on Trakt.");
+
+                var traktMovie = traktMovies.FirstOrDefault(x => HasMatchingId(plexMovie, x.Ids));
+
+                if (traktMovie != null)
+                {
+                    // Add to the processed list to find extra items.
+                    processedMovies.Add(traktMovie);
+                }
+
+                if (plexMovie.ViewCount > 0)
+                {
+                    if (traktMovie == null)
+                    {
+                        await ReportProgressAsync($"The movie \"{plexMovie.Title}\" was not found on Trakt. Skipping");
+                    }
+                    else
+                    {
+                        // Found it and is not watched, lets update this.
+                        TraktSyncHistoryPostBuilder spb = new TraktSyncHistoryPostBuilder();
+
+                        spb.AddMovie(traktMovie);
+
+                        await TraktClient.Sync.AddWatchedHistoryItemsAsync(spb.Build());
+
+                        await ReportProgressAsync($"The movie \"{plexMovie.Title}\" was set as watched on Trakt");
+                    }
+                }
+                else
+                {
+                    await ReportProgressAsync($"The movie \"{plexMovie.Title}\" was watched on Trakt or plex.");
+                }
             }
             else
             {
+                // Add to the processed list to find extra items.
+                processedMovies.Add(traktMovieWatched);
+
                 await ReportProgressAsync($"Found the movie \"{plexMovie.Title}\" as watched on Trakt. Processing!");
 
                 if (plexMovie.ViewCount > 0)
